c1387418ed90b4fd7b0dd60316cdd2429a6ba249
paolobrusa <paolo2.brusa@mail.polimi.it>
2025-06-02 16:16:51 +0200

    continue with toggle airoute

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +7,11 @@
 typedef struct {
     int x, y;
 } Coord;
+
+typedef struct {
+    Coord value1;
+    int routeCost;
+} valueAir;
 
 typedef struct Entry {
     Coord key;
@@ -16,6 +22,16 @@ typedef struct Entry {
 typedef struct {
     Entry** buckets;
 } HashMapPosition;
+
+typedef struct EntryAiroute {
+    Coord key;
+    valueAir value[5];
+    struct EntryAiroute* next;
+} EntryAiroute;
+
+typedef struct {
+    EntryAiroute** bucketsAir;
+} HashMapAiroute;
 
 //Funzione hash
 unsigned int hashing(Coord coord, int matrixSize) {
@@ -48,6 +64,37 @@ HashMapPosition* create(int matrixSize, int raw) {
     return map;
 }
 
+HashMapAiroute* create2(int matrixSize) {
+    HashMapAiroute* map = malloc(sizeof(HashMapAiroute));
+    map->bucketsAir = calloc(matrixSize/2, sizeof(EntryAiroute*));
+    return map;
+}
+
+void insertRoute(HashMapAiroute* map, Coord key, Coord value, int value2, int matrixSize) {
+    unsigned int index = hashing(key, matrixSize/2);
+    EntryAiroute* entry = map->bucketsAir[index];
+    while (entry != NULL) {
+        if (checkKey(entry->key, key)) {
+            for (int i = 0; i < 5; i++) {
+                if (checkKey(entry->value[i].value1, value)) {
+                    entry->value[i].routeCost = value2;
+                    return;
+                }
+                //cosa ci sara dentro entry value?
+            }
+        }
+        entry = entry->next;
+    }
+    entry = malloc(sizeof(EntryAiroute));
+    entry->key = key;
+    for (int i = 0; i < 5; i++) {
+        entry->value[i].value1 = value;
+        entry->value[i].routeCost = value2;
+    }
+    entry->next = map->bucketsAir[index];
+    map->bucketsAir[index] = entry;
+}
+
 void modifyCoord(HashMapPosition* map, Coord key, int value, int matrixSize) {
     unsigned int index = hashing(key, matrixSize);
     Entry* entry = map->buckets[index];
@@ -74,7 +121,7 @@ int getPair(HashMapPosition* map, Coord key, int* found, int matrixSize) {
     return 0;
 }
 
-void delete(HashMapPosition* map, int matrixSize) {
+void deletePos(HashMapPosition* map, int matrixSize) {
     for (int i = 0; i < matrixSize; i++) {
         Entry* entry = map->buckets[i];
         while (entry != NULL) {
@@ -87,10 +134,35 @@ void delete(HashMapPosition* map, int matrixSize) {
     free(map);
 }
 
-HashMapPosition* init(int y, int x, HashMapPosition* map) {
+void deleteAiroute(HashMapAiroute* map, int matrixSize) {
+    for (int i = 0; i < matrixSize/2; i++) {
+        EntryAiroute* entry = map->bucketsAir[i];
+        while (entry != NULL) {
+            EntryAiroute* next = entry->next;
+            free(entry);
+            entry = next;
+        }
+    }
+    free(map->bucketsAir);
+    free(map);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+HashMapPosition* init(int y, int x, HashMapPosition* map, int matrixSizeOld) {
     int matrixSize = y * x;
     if (map != NULL) {
-        delete(map, matrixSize);
+        deletePos(map, matrixSizeOld);
     }
     return create(matrixSize, x);
 }
@@ -104,12 +176,22 @@ HashMapPosition* init(int y, int x, HashMapPosition* map) {
 
 
 
-int main(void) {
-    HashMapPosition* map = init(10, 10, NULL);
-    int found;
-    Coord testKey = {1, 1};
-    modifyCoord(map, testKey, 4, 100);
-    printf("%d\n",getPair(map, testKey, &found, 100));
-    delete(map, 100);
+int main(int argc, const char *argv[]) {
+    HashMapPosition* map = NULL;
+    int x, y, precX, precY;
+    while (1) {
+        precX = x;
+        precY = y;
+        scanf("%d %d", &x, &y);
+        if (x == -1 && y == -1) {
+            break;
+        }
+        map = init(y, x, map, precX * precY);
+        int found = 0;
+        Coord key = {1, 1};
+        int value = getPair(map, key, &found, x * y);
+        printf("%d\n", value);
+    }
+    deletePos(map, precX * precY);
     return 0;
 }
